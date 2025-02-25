c1a53c16d656e822b0fbdb02298bb016759c8ff4
Sabu Rana <saburana@Sabus-MacBook-Pro.local>
2025-02-25 20:42:19 +0545

    Add URL patterns for home, signup, login, and logout in myapp

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
