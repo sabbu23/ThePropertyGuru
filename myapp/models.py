00d37ab57a1396c30a887f2a3806dcdc6d1a4966
Sabu Rana <saburana@Sabus-MacBook-Pro.local>
2025-02-25 19:57:01 +0545

    Add UserProfile model in models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# for adding additional user fields later, a profile model can be created:
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
