00175ccb06c76b5596f34b133a0999aa370f7eea
Sabu Rana <saburana@Sabus-MacBook-Pro.local>
2025-02-28 10:19:37 +0545

    Updated logout functionality to redirect to home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
